cb50e4ea6141c53896bbba583da7cf2de8752146
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-12 23:29:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.ebay.com/itm/Shark-DuoClean-Powered-Lift-Away-Speed-Upright-Vacuum-Cinnamon-Red-NV803/312123597109?_trkparms=pageci%3A53e6d18f-1693-11e9-8553-74dbd18053b5%7Cparentrq%3A4334d9171680aadcb246e17efff5658f%7Ciid%3A1">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.ebay.com/itm/Shark-DuoClean-Powered-Lift-Away-Speed-Upright-Vacuum-Cinnamon-Red-NV803/312123597109?_trkparms=pageci%3A53e6d18f-1693-11e9-8553-74dbd18053b5%7Cparentrq%3A4334d9171680aadcb246e17efff5658f%7Ciid%3A1"
         </script>
         <title>Page Redirection</title>
     </head>
